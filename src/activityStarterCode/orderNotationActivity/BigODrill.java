5e8c13c098a85187a4185465e66b3d51a11d40d1
kadioglu <kadioglu@gmail.com>
2020-01-28 22:33:54 -0600

    Add files via upload

--- src/activityStarterCode/orderNotationActivity/BigODrill.java
+++ src/activityStarterCode/orderNotationActivity/BigODrill.java
@@ -14,7 +14,7 @@ public class BigODrill {
      * @return the last element of the array numArr
      */
     public static int constantTime(int[] numArr) {
-        //TODO: Add the code that retuns the last element of the input array with O(1) run time
+        //TODO: Add the code that returns the last element of the input array with O(1) run time
 
     }
 
@@ -26,17 +26,19 @@ public class BigODrill {
      * @return the input array where each element of the array is multiplied with itself
      */
     public static int[] linearTime(int[] numArr) {
-        //TODO: Add the code modify and return the input array with each element squared
+        //TODO: Add the code to modify and return the input array with each element squared
         // which performs in O(n)
 
+        return numArr;
     }
 
     /**
      * (3)
-     * Demonstrates an algorithm with TODO: What is O( ) of the following code?
+     * Demonstrates an algorithm with TODO: What is O( ? ) of the following code?
      * @param numArr - an integer array
      */
     public static int[] puzzle03(int[] numArr) {
+        //TODO: What does the following code do?
         int index2 = 0;
         int[] tempArr = new int[numArr.length];
         for(int index = 1; index < numArr.length; index = index * 2) {
@@ -48,19 +50,20 @@ public class BigODrill {
 
     /**
      * (4)
-     * Demonstrates an algorithm with TODO: What is the O( ) of the following code?
+     * Demonstrates an algorithm with TODO: What is the O( ? ) of the following code?
      * @param numArr -  a two dimensional rectangular integer array a.k.a 2D matrix
      * @return a double that represents the sparsity of numArr
      */
-    public double sparsity(int[][] numArr) {
+    public static double sparsity(int[][] numArr) {
         //TODO: Write the code that calculates and returns the sparsity of the input
         // rectangular integer array numArr
+
 
     }
 
     /**
      * (5)
-     * Demonstrates an algorithm with TODO: What is the O( ) of the following code?
+     * Demonstrates an algorithm with TODO: What is the O( ? ) of the following code?
      * @param numArr - two dimensional rectangular integer array a.k.a 2D matrix
      * @param sparsityValue - 0.0-1.0 representing sparsity to make the matrix.
      * @return - numArray overwritten as a sparse matrix with a sparsity of sparsityValue
@@ -70,7 +73,14 @@ public class BigODrill {
         Random randGenerator = new Random();
 
         //TODO: Write the code to overwrite the numArr such that it is a sparse matrix
-        // with a sparsity of sparsityValue
+        // with a sparsity of %75
+
+        return numArr;
+    }
+
+
+    public static void main (String[] args){
+        //TODO: Write code to run your methods
 
     }
 }
