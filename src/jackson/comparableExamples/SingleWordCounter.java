de0938a67ad098d92fcd9ba3cdcecf16651ecf8e
bjackson <bjackson@macalester.edu>
2020-01-31 09:07:26 -0600

    Added Comparable example for section 1

---
+++ src/jackson/comparableExamples/SingleWordCounter.java
@@ -0,0 +1,22 @@
+package jackson.comparableExamples;
+
+public class SingleWordCounter implements Comparable<SingleWordCounter> {
+    private String word;
+    private int count;
+
+    SingleWordCounter(){}
+    //inc count
+    // get count
+
+    public int compareTo(SingleWordCounter other){
+        if (this.count < other.count){
+            return -1;
+        }
+        else if (this.count > other.count){
+            return  1;
+        }
+        else {
+            return word.compareTo(other.word);
+        }
+    }
+}
